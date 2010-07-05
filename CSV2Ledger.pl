e0eb75f4cb4dbf33a782c027b59c175fe1103a70
Vinod Kurup <vinod@kurup.com>
2010-07-04 21:16:42 -0400

    merged vendor into master

--- CSV2Ledger.pl
+++ CSV2Ledger.pl
@@ -42,6 +42,27 @@
 #  (CSV2Ledger:MD5Sum)   ; MD5Sum: .......
 #  (CSV2Ledger:CSV)      ; CSV: csv data
 #
+#
+#
+#
+#######################################################################
+# New TT support on a per-file basis, now the FileMatches.yaml file
+# can contain a TT template to override the default.
+#
+# The variables available are from TempTrans, including:
+# $TempTrans: {
+#               Amount => -90.00,
+#               CSV => '"12/21/2009","12/19/2009","Debit","CAB SERVIC HOUSTON, TX","Travel",$90.00',
+#               CheckNum => '(12/21/2009) ',
+#               Cleared => '',
+#               Date => '2009/12/19',
+#               Desc => 'CAB SERVIC HOUSTON, TX',
+#               Dest => 'UnknownImported',
+#               FilesList => '/home/rladams/ScannedDocuments/Receipts/20091218_Taxi_90_00.jpg',
+#               MD5Sum => '8cba5105c58c415bd4b1d6acb15b20bf',
+#               Source => 'Source:AISVisa'
+#             }
+#
 ##################################################
 
 # Always
@@ -50,10 +71,11 @@ use warnings;
 
 # Modules
 use YAML;            # Dispatch Table Loading
-use Getopt::Long qw(:config bundling);    # Options processing
+use Getopt::Long;    # Options processing
 use File::Find;
 use Date::Format;    # Date conversion
 use Date::Parse;
+use Date::Calc qw/Localtime Add_Delta_Days/;
 use Smart::Comments -ENV, "###";    # Ignore my headers
 use Digest::MD5 qw/md5_hex/;        # Help detect duplicates later
 
@@ -100,17 +122,47 @@ my $MatchedFiles = 0;
     'DefaultSource' => "Assets:Unknown",      # What default source account?
 
     # Include default files now if they exist in the current dir
-    'FileMatchFile' => -e "./FileMatches.yaml"
-    ? "./FileMatches.yaml"
+    'FileMatchFile' => -e "./.FileMatches.yaml"
+    ? "./.FileMatches.yaml"
     : undef,
 
-    'AccountMatchFile' => -e "./AccountMatches.yaml"
-    ? "./AccountMatches.yaml"
+    'AccountMatchFile' => -e "./.AccountMatches.yaml"
+    ? "./.AccountMatches.yaml"
     : undef,
 
-    'PreProcessFile' => -e "./PreProcess.yaml"
-    ? "./PreProcess.yaml"
+    'PreProcessFile' => -e "./.Preprocess.yaml"
+    ? "./.Preprocess.yaml"
     : undef,
+
+
+    # Configurable in FileMatches.yaml, like this example with metadata!
+    # TxnOutputTemplate: "[% USE format %][% ToDollars = format('%30.2f') %][% Date %] [% Cleared %][% CheckNum %][% Desc %]\n    ; RECEIPT: [% FilesList %]\n    ; ER: Unfiled\n    ; PROJECT: Unfiled\n    ; OWNER: Adams, Russell\n    [% Source %]	[% ToDollars(Amount) %]\n    [% Dest %]\n    (CSV2Ledger:MD5Sum)   ; MD5Sum: [% MD5Sum  %]\n    (CSV2Ledger:CSV)      ; CSV: [% CSV %]\n\n\n"
+
+    'TxnOutputTemplate' => <<EOF
+[% USE format %][% ToDollars = format('\$%.2f') %][% IndentDollars = format('%40s') %][% Date %] [% Cleared %][% CheckNum %][% Desc %]
+    ; RECEIPT: [% FilesList %]
+    ; CSV: [% CSV %]
+    ; MD5Sum: [% MD5Sum  %]
+    [% Source %]	[% IndentDollars(FormattedAmount) %]
+    [% Dest %]
+        ; ER: [% ER %]
+        ; PROJECT: [% PROJECT %]
+        ; CATEGORY: [% Category %]
+
+
+EOF
+            ,
+
+
+# 2.6 style
+#     'TxnOutputTemplate' => " [% USE format %][% ToDollars = format('%30.2f') %]
+# [% Date %] [% Cleared %][% CheckNum %][% Desc %] [% FilesList %]
+# 	[% Source %]	[% ToDollars(Amount) %]
+# 	[% Dest %]
+# 	(CSV2Ledger:MD5Sum)   ; MD5Sum: [% MD5Sum  %]
+# 	(CSV2Ledger:CSV)      ; CSV: [% CSV %]
+
+# ",
 
     # Simple options
     'ShowHelp'   => undef,
@@ -121,7 +173,11 @@ my $MatchedFiles = 0;
     'FindDir'    => ".",
     'InputFile'  => undef,
     'OutputDir'  => ".",
-    'OutputFile' => "CSV2Ledger.dat",
+    'OutputFile' => "./CSV2Ledger.dat",
+    'MD5Cache'   => "./.MD5Sum.cache",
+    'FuzzyDates' => 3,
+    'MetaData'   => { "ER" => "Unassigned",
+                      "PROJECT" => "Unassigned" },
 
 );
 
@@ -145,11 +201,14 @@ GetOptions(
     'X'      => \$Options{'FindFiles'},
     'G=s'    => \$Options{'FindDir'},
     'f=s'    => \$Options{'FileMatchFile'},
+    'F=i'    => \$Options{'FuzzyDates'},
     'p=s'    => \$Options{'PreProcessFile'},
     'm=s'    => \$Options{'AccountMatchFile'},
+    'M=s'    => \$Options{'MD5Cache'},
     'i=s'    => \$Options{'InputFile'},
     'o=s'    => \$Options{'OutputFile'},
     'D=s'    => \$Options{'OutputDir'},
+    'E=s%'    => \$Options{'MetaData'},
     'h|help' => \$Options{'ShowHelp'}
 ) || die 'Failed to get options';
 
@@ -174,9 +233,12 @@ Options:
  -X                       Turn on file location
  -G <directory>           Source directory for files search
  -s "Account"             Default Source Account
+ -F #                     Fuzzy match file dates by X days (3 default)
  -f <file>                File matching table YAML file
  -p <file>                Preprocess table YAML file
  -m <file>                Account matching table YAML file
+ -M <file>                MD5 sum cache
+ -E KEY=VALUE             Add metadata
 
 EOF
     exit;
@@ -245,14 +307,16 @@ sub AccountMatch {
 
           # Return first match
           return {
-            'Source' => $CurRe->[1],
-            'Dest'   => $CurRe->[2]
+            'Source'   => $CurRe->[1],
+            'Dest'     => $CurRe->[2],
+            'Category' => $CurRe->[3]
           };
     }
 
     return {
-        'Source' => $Options{'DefaultSource'},
-        'Dest'   => 'Expense:Unknown'
+        'Source'   => $Options{'DefaultSource'},
+        'Dest'     => 'Expense:Unknown',
+        'Category' => 'Unknown'
     };
 
 }
@@ -361,6 +425,8 @@ while (<FH>) {
 
     chomp;
 
+    s/\r//g;
+
     #### Original CSV: $_
 
     # Only process records matching the RecordRE
@@ -407,9 +473,15 @@ while (<FH>) {
         'Desc'     => $Transaction{ $Options{'DescField'} },
         'Amount'   => $Transaction{ $Options{'AmountField'} } *
           $Options{'Negate'},
-        'MD5Sum' => md5_hex($OrigCSV)    # later dup detect
+        'MD5Sum'   => md5_hex($OrigCSV),    # later dup detect
+        'MD5SumCR' => md5_hex($OrigCSV . "\r")    # urg, i left cr on old records, match either
         , 'CSV' => $OrigCSV              # reference
     };
+
+    map { $TempTrans->{$_} = $Options{'MetaData'}{$_} }  keys %{$Options{'MetaData'}};
+
+    # Prettify the amount
+    $TempTrans->{'FormattedAmount'} = sprintf("\$%0.2f",$TempTrans->{'Amount'});
 
     # Match our source / dest accounts
     $TempTrans = { %{$TempTrans}, %{ &AccountMatch($_) } };
@@ -417,6 +489,11 @@ while (<FH>) {
     # Set to default if undefined
     if ( !defined $TempTrans->{'Source'} ) {
         $TempTrans->{'Source'} = $Options{'DefaultSource'};
+    }
+
+    # Set to default if undefined
+    if ( !defined $TempTrans->{'Category'} ) {
+        $TempTrans->{'Category'} = 'Unknown';
     }
 
     # Set to default if empty
@@ -458,6 +535,53 @@ while (<FH>) {
 
             close DUP;
 
+            # Open output file
+            open( DUP, "<" . "$Options{'OutputFile'}" )
+              || die "Failed to open $Options{'OutputFile'} for reading";
+
+            # Grep the output file for the MD5 sum, skip if present.
+            if ( grep( /$TempTrans->{'MD5SumCR'}/, <DUP> ) ) {
+                close DUP;
+                print STDERR "Skipping $TempTrans->{'MD5Sum'}\n";
+                $Skipped++;
+                next;
+            }
+
+            close DUP;
+
+        };
+
+        # If the cache file doesn't exist, don't sweat it
+        ( -f $Options{'MD5Cache'} ) && do {
+
+            # Open output file
+            open( DUP, "<" . "$Options{'MD5Cache'}" )
+              || die "Failed to open $Options{'MD5Cache'} for reading";
+
+            # Grep the output file for the MD5 sum, skip if present.
+            if ( grep( /$TempTrans->{'MD5Sum'}/, <DUP> ) ) {
+                close DUP;
+                print STDERR "Skipping $TempTrans->{'MD5Sum'}\n";
+                $Skipped++;
+                next;
+            }
+
+            close DUP;
+
+            # Open output file
+            open( DUP, "<" . "$Options{'MD5Cache'}" )
+              || die "Failed to open $Options{'MD5Cache'} for reading";
+
+            # Grep the output file for the MD5 sum, skip if present.
+            if ( grep( /$TempTrans->{'MD5SumCR'}/, <DUP> ) ) {
+                close DUP;
+                print STDERR "Skipping $TempTrans->{'MD5Sum'}\n";
+                $Skipped++;
+                next;
+            }
+
+            close DUP;
+
         };
 
     };
@@ -468,36 +592,57 @@ while (<FH>) {
     # Attach files that match the txn to the payee.
     # Filenames should be in the form: YYYYMMDD_Vendor_Dollars_Cents.*$
     # Vendor name isn't likely to match, so lets focus on the date and dollars
-    # Then we use the <<file:/dir/filename [,file:...] >> attachment syntax
+    # Then we use the /dir/filename [,file:...] attachment syntax
 
     my $TempAmount = sprintf( "%.2f", $Transaction{ $Options{'AmountField'} } );
     $TempAmount =~ tr/./_/;
 
-    my $SearchRegexp =
-        time2str( "%Y%m%d", str2time( $Transaction{ $Options{'DateField'} } ) )
-      . '_' . '.*?' . '_'
-      . $TempAmount . '[_.]';
+# Date::Calc::Localtime returns ($year,$month,$day, $hour,$min,$sec, $doy,$dow,$dst)
+    my @DateFieldCalc =
+      ( Localtime( str2time( $Transaction{ $Options{'DateField'} } ) ) )
+      [ 0, 1, 2 ];
 
-    #### $SearchRegexp
+    # Seed with proper date
+    my @SearchRegexp = ( sprintf( "%04d%02d%02d", @DateFieldCalc)
+                                  . '_' . '.*?' . '_' . $TempAmount . '[_.]' );
+
+    # Fuzz days before or after...
+    foreach
+      my $FuzzFactor ( $Options{'FuzzyDates'} * -1 .. $Options{'FuzzyDates'} )
+        {
+        push(
+            @SearchRegexp, sprintf( "%04d%02d%02d",
+                Add_Delta_Days( @DateFieldCalc, $FuzzFactor ) )
+            . '_' . '.*?' . '_' . $TempAmount . '[_.]'
+        );
+    }
+
+    #### @SearchRegexp
 
     my $FilesList = "";
+    my $Found = 0;
 
     find(
         sub {
-            if (/$SearchRegexp/) {
-                if ($FilesList) { $FilesList .= ','; }
-                $FilesList .= 'file:' . $File::Find::name;
-                print STDERR "Matched $_ to "
-                  . $Transaction{ $Options{'DateField'} } . " "
-                  . $Transaction{ $Options{'DescField'} } . " "
-                  . $Transaction{ $Options{'AmountField'} } . "\n";
-                $MatchedFiles++;
+            foreach my $RE (@SearchRegexp) {
+                unless ($Found) {
+                    if (/$RE/) {
+                        if ($FilesList) { $FilesList .= ','; }
+                        my $TempName = $File::Find::name;
+                        $TempName =~ s/$Options{'FindDir'}//g;
+                        $FilesList .= $TempName;
+                        print STDERR "Matched $_ to "
+                          . $Transaction{ $Options{'DateField'} } . " "
+                          . $Transaction{ $Options{'DescField'} } . " "
+                          . $Transaction{ $Options{'AmountField'} } . "\n";
+                        $MatchedFiles++;
+                        $Found++;
+                    }
+                }
             }
         },
         $Options{'FindDir'}
     );
-
-    if ($FilesList) { $FilesList = '<<' . $FilesList . '>>'; }
 
     ########################################
     # Output
@@ -505,17 +650,8 @@ while (<FH>) {
     # Add fileslist for tempate
     $TempTrans->{'FilesList'} = $FilesList;
 
-    my $TxnOutputTemplate = <<EOF;
-[% USE format %][% ToDollars = format('%30.2f') %]
-[% Date %] [% Cleared %][% CheckNum %][% Desc %] [% FilesList %]
-	[% Source %]	[% ToDollars(Amount) %]
-	[% Dest %]
-	(CSV2Ledger:MD5Sum)   ; MD5Sum: [% MD5Sum  %]
-	(CSV2Ledger:CSV)      ; CSV: [% CSV %]
 
-EOF
-
-    $TT->process( \$TxnOutputTemplate, $TempTrans,
+    $TT->process( \$Options{'TxnOutputTemplate'}, $TempTrans,
         ">" . "$Options{'OutputFile'}" );
 
 }
